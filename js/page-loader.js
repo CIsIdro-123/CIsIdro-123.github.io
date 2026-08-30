bfad9284cbd3ec4da882e4e1d0febd48b7a5eef8
Clsldro-123 <2512031132@stu.jit.edu.cn>
2026-08-30 23:24:24 +0800

    Site updated: 2026-08-30 23:24:23

--- js/page-loader.js
+++ js/page-loader.js
@@ -1,27 +1,6 @@
 (function () {
-    const loader = document.getElementById('page-loader');
+    var loader = document.getElementById('page-loader');
     if (!loader) return;
-
-    // 根据路径切换 GIF
-    const path = window.location.pathname;
-    const gifMap = {
-        '/tags/':      '/img/loading-tags.gif',
-        '/archives/':  '/img/loading-archives.gif',
-        '/about/':     '/img/loading-about.gif',
-        '/guestbook/': '/img/loading-guestbook.gif',
-    };
-
-    const gifEl = loader.querySelector('.loader-gif');
-    if (gifEl) {
-        let matched = '/img/loading-default.gif';
-        for (const [key, val] of Object.entries(gifMap)) {
-            if (path.startsWith(key) || path === key) {
-                matched = val;
-                break;
-            }
-        }
-        gifEl.src = matched;
-    }
 
     window.addEventListener('load', function () {
         setTimeout(function () {
@@ -31,6 +10,6 @@
                     loader.parentNode.removeChild(loader);
                 }
             }, 600);
-        }, 400);
+        }, 300);
     });
 })();
